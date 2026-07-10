82e2a110658fd08d411be32c249b35037be86ab4
Acton Chen <zID@ad.unsw.edu.au>
2026-07-10 22:38:46 +1000

    fixed server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors'
-import crypto from 'crypto'
+import crypro from 'crypto'
 
 // Set up web app
 const app = express()
